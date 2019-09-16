e162630a1fd0780ba89b31df78996e6c7c4e3e9e
Elton Stoneman <elton@sixeyed.com>
2019-09-16 20:49:19 +0100

    Parameterize file name in secret

--- actions/aks-create-secret/setup.sh
+++ actions/aks-create-secret/setup.sh
@@ -5,9 +5,10 @@
 # AZURE_SP_TENANT
 # AZURE_SP_PASSWORD
 # AZURE_RESOURCE_GROUP
-# AZURE_REGION
 # AKS_CLUSTER_NAME
 # AKS_SECRET_NAME
+# AKS_SECRET_FILE_NAME
+# AKS_SECRET_FILE_VALUE
 
 # login
 az login --service-principal -u $AZURE_SP_NAME -p $AZURE_SP_PASSWORD --tenant $AZURE_SP_TENANT > /dev/null
@@ -17,5 +18,5 @@ az aks get-credentials --resource-group $AZURE_RESOURCE_GROUP --name $AKS_CLUSTE
     
 echo "Creating secret: $AKS_SECRET_NAME"
 mkdir -p /tmp/aks
-echo $AKS_SECRET_VALUE > /tmp/aks/secret
-kubectl create secret generic $AKS_SECRET_NAME --from-file=/tmp/aks/secret
+echo $AKS_SECRET_FILE_VALUE > /tmp/aks/$AKS_SECRET_FILE_NAME
+kubectl create secret generic $AKS_SECRET_NAME --from-file=/tmp/aks/$AKS_SECRET_FILE_NAME
